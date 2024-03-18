6ea2fa493aef47495b2a8bf773b30e9d3efc6577
Michelangelo Balzano <m.balzano14@studenti.uniba.it>
2024-03-18 16:25:25 +0100

    Stampa grafici

--- pretraining/training_methods.py
+++ pretraining/training_methods.py
@@ -41,11 +41,13 @@ def train_model(model, dataloader, batch_size, optimizer, device):
         # Passaggio del batch al modello
         output_segments = model(masked_batch)
 
-        # Stampa i grafici 
-        segment_data = {signal: data[0] for signal, data in batch.items()}
-        segment_outputs = {signal: data[0] for signal, data in output_segments.items()}
-        stampa_grafico(segment_data, segment_outputs, masks[0])
-
+        # Stampa i grafici
+        '''
+        for i in range(32):
+            segment_data = {signal: data[i] for signal, data in batch.items()}
+            segment_outputs = {signal: data[i] for signal, data in output_segments.items()}
+            stampa_grafico(segment_data, segment_outputs, masks[i])
+        '''
         # Calcolo della loss
         loss = masked_prediction_loss(output_segments, batch, masks, batch_size)
 
@@ -103,9 +105,12 @@ def early_stopping(val_losses, patience=10):
 
 # Metodo provvisorio per la stampa dei dati di un singolo segmento
 def stampa_grafico(segment_data, outputs, mask):
-    time = np.arange(240) / 4
 
-    print(mask.size())
+    print(segment_data['bvp'])
+    print(outputs['bvp'])
+    print(mask)
+
+    time = np.arange(240) / 4
 
     zero_intervals = []
     start_idx = None
@@ -125,26 +130,35 @@ def stampa_grafico(segment_data, outputs, mask):
 
     plt.subplot(3, 1, 1)
     plt.plot(time, segment_data['bvp'].cpu().numpy(), color='blue')
-    for start, end in zero_intervals:
-        plt.fill_between(time[start:end+1], segment_data['bvp'][start:end+1].cpu().numpy().flatten(), color='gray')
     plt.plot(time, outputs['bvp'].detach().cpu().numpy().flatten(), color='red')
+    min = np.min([np.min(segment_data['bvp'].cpu().numpy()[:, 0]), np.min(outputs['bvp'].detach().cpu().numpy()[:, 0])])
+    max = np.max([np.max(segment_data['bvp'].cpu().numpy()[:, 0]), np.max(outputs['bvp'].detach().cpu().numpy()[:, 0])])
+    y_values = [min, max]
+    for start, end in zero_intervals:
+        plt.fill_betweenx(y_values, start/4, end/4, color='grey')
     plt.title('Blood Volume Pulse (BVP)')
     plt.xlabel('Tempo (s)')
     plt.ylabel('BVP')
 
     plt.subplot(3, 1, 2)
     plt.plot(time, segment_data['eda'].cpu().numpy(), color='blue')
-    for start, end in zero_intervals:
-        plt.fill_between(time[start:end+1], segment_data['eda'][start:end+1].cpu().numpy().flatten(), color='gray')
     plt.plot(time, outputs['eda'].detach().cpu().numpy().flatten(), color='red')
+    min = np.min([np.min(segment_data['eda'].cpu().numpy()[:, 0]), np.min(outputs['eda'].detach().cpu().numpy()[:, 0])])
+    max = np.max([np.max(segment_data['eda'].cpu().numpy()[:, 0]), np.max(outputs['eda'].detach().cpu().numpy()[:, 0])])
+    y_values = [min, max]
+    for start, end in zero_intervals:
+        plt.fill_betweenx(y_values, start/4, end/4, color='grey')
     plt.title('Electrodermal Activity (EDA)')
     plt.xlabel('Tempo (s)')
     plt.ylabel('EDA')
 
     plt.subplot(3, 1, 3)
     plt.plot(time, segment_data['hr'].cpu().numpy(), color='blue')
+    min = np.min([np.min(segment_data['hr'].cpu().numpy()[:, 0]), np.min(outputs['hr'].detach().cpu().numpy()[:, 0])])
+    max = np.max([np.max(segment_data['hr'].cpu().numpy()[:, 0]), np.max(outputs['hr'].detach().cpu().numpy()[:, 0])])
+    y_values = [min, max]
     for start, end in zero_intervals:
-        plt.fill_between(start, end, color='gray')
+        plt.fill_betweenx(y_values, start/4, end/4, color='grey')
     plt.plot(time, outputs['hr'].detach().cpu().numpy().flatten(), color='red')
     plt.title('Heart Rate (HR)')
     plt.xlabel('Tempo (s)')
